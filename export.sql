acbbca060d50e849f1a49f3f6cec3d79c56e5fea
thesivis <raphael@ic.ufmt.br>
2019-05-04 09:04:10 -0400

    Ajustando export

--- export.sql
+++ export.sql
@@ -1,8 +1,8 @@
-\COPY (SELECT * FROM inverno_2011_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/inverno_2011_fim.csv' DELIMITER ',' CSV HEADER;
-\COPY (SELECT * FROM inverno_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/inverno_2016_fim.csv' DELIMITER ',' CSV HEADER;
-\COPY (SELECT * FROM primavera_2011_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/primavera_2011_fim.csv' DELIMITER ',' CSV HEADER;
-\COPY (SELECT * FROM primavera_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/primavera_2016_fim.csv' DELIMITER ',' CSV HEADER;
-\COPY (SELECT * FROM outono_2011_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/outono_2011_fim.csv' DELIMITER ',' CSV HEADER;
-\COPY (SELECT * FROM outono_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/outono_2016_fim.csv' DELIMITER ',' CSV HEADER;
-\COPY (SELECT * FROM verao_2012_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/verao_2012_fim.csv' DELIMITER ',' CSV HEADER;
-\COPY (SELECT * FROM verao_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosFinal/verao_2016_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM inverno_2011_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosComSolo/inverno_2011_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM inverno_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosComSolo/inverno_2016_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM primavera_2011_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosComSolo/primavera_2011_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM primavera_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosComSolo/primavera_2016_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM outono_2011_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosComSolo/outono_2011_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM outono_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadDadosComSoloosFinal/outono_2016_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM verao_2012_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosComSolo/verao_2012_fim.csv' DELIMITER ',' CSV HEADER;
+\COPY (SELECT * FROM verao_2016_fim) TO '/home/raphael/Codigo/git/theprojects/Diana/DadosComSolo/verao_2016_fim.csv' DELIMITER ',' CSV HEADER;
